ed7343ac20aabe7785151550629fd10697f2d4bd
HSC98 <gktmdcjf98@gmail.com>
2024-08-12 17:48:50 +0900

    수정본

--- js/chelsea.js
+++ js/chelsea.js
@@ -3,7 +3,7 @@ $(function(){
         $(".s_bg, .sub").stop().slideDown();
     })
 
-    $(".main > li").mouseout(function(){
+    $(".main > li, .s_bg").mouseout(function(){
         $(".s_bg, .sub").stop().slideUp();
     })
 
